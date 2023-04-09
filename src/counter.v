580146a3f2a7686889fa7b645591fff233d42344
Kevin Guan <kevinwguan@protonmail.com>
2023-04-09 03:41:31 -0700

    counter def

--- src/counter.v
+++ src/counter.v
@@ -1,4 +1,4 @@
-module counter (input rst, clk, output reg [2:0] count);
+module counter (input rst, input clk, output reg [2:0] count);
    always @(posedge clk, rst) begin
       if (rst == 1)
           begin
