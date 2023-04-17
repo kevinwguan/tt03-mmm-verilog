21e151970eefc0e6f7317b442a060015b92cb4ab
Kevin Guan <kevinwguan@protonmail.com>
2023-04-17 00:30:02 -0700

    bug fix

--- src/test.py
+++ src/test.py
@@ -6,12 +6,12 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_kevinwguan(dut):
     dut._log.info("start")
-    clock = Clock(dut.clk, 80, units="us")
+    clock = Clock(dut.ext_clk, 80, units="us")
     cocotb.start_soon(clock.start())
 
     dut._log.info("reset")
     dut.rst.value = 1
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.ext_clk, 10)
     dut.rst.value = 0
 
     #assert int(dut.segments.value) == segments[i]
